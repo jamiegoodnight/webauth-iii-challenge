78ea7a59f419e06bd7050c805be6a689ad81bce3
Jamie Goodnight <goodnightlambda@gmail.com>
2019-04-03 15:06:25 -0400

    Composed users table, knexfile, index, and server.

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+const authorizationRouter = require("./authorization/authorizationRouter");
+const usersRouter = require("./users/usersRouter");
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use("/api/auth", authorizationRouter);
+server.use("/api/users", usersRouter);
+
+module.exports = server;
